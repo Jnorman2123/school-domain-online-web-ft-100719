0dc11827bf2b0cf0b53852f7d9fcb633e653e8bf
Jonathan Norman <jnorman2123@gmail.com>
2019-10-14 15:18:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,8 +12,8 @@ class School
   end
 
   def add_student(student_grade, student_name)
-    @roster[student_grade] = []
-    @roster[student_grade] << student_name
+    @roster[:student_grade] = []
+    @roster[:student_grade] << student_name
   end
 
 end
