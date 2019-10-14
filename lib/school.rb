c6727b9e62fca42f7a79d6cb5dd6a1809e5402b5
Jonathan Norman <jnorman2123@gmail.com>
2019-10-14 15:46:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
 
   def add_student(student_name, student_grade)
     if roster[student_grade]
-      roster[student_grade] << [student_name]
+      roster[student_grade] << student_name
     else
       roster[student_grade] = [student_name]
     end
