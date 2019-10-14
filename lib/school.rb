9e10c8169cbb7641a315005b985d74e2e6a87ccf
Jonathan Norman <jnorman2123@gmail.com>
2019-10-14 15:14:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+class School
+  attr_accessor :student
+
+  def initialize(name)
+    @name
+    roster
+  end
+
+  def roster
+    @roster = {}
+  end
+
+  def add_student(student_grade, student_name)
+    roster[student_grade] = []
+    roster[student_grade] << student_name
+  end
+
+end
